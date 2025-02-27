5835890237e455bddf2fbd3d63d8c87041ee474d
ll <xtfrglll@gmail.com>
2025-02-27 12:42:56 +0800

    Fix static files

--- calendar_app/settings.py
+++ calendar_app/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'calendar_notes', 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'calendar_notes/static')]
 # Для продакшена:
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
