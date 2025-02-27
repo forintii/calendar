145b69b6d13d1c5d5866e1d24824466f6eadc117
ll <xtfrglll@gmail.com>
2025-02-27 12:11:08 +0800

    Добавлен CSRF_TRUSTED_ORIGINS для Railway

--- calendar_app/settings.py
+++ calendar_app/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'calendar_note/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'calendar_notes', 'static')]
 # Для продакшена:
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
